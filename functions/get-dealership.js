4692e3b20963956133c64b9d3259bc0e92224cdb
David Yeung <sdyeung@gmail.com>
2024-01-28 03:17:24 -0500

    add dealerships list

--- functions/get-dealership.js
+++ functions/get-dealership.js
@@ -6,8 +6,8 @@ const Cloudant = require('@cloudant/cloudant');
 async function dbCloudantConnect() {
     try {
         const cloudant = Cloudant({
-            plugins: { iamauth: { iamApiKey: '' } }, // Replace with your IAM API key
-            url: '', // Replace with your Cloudant URL
+            plugins: { iamauth: { iamApiKey: 'Z4p8sg3DYT7tKri0hPkhQXBxa9CWfx_uaeBPF29IBeiK' } }, // Replace with your IAM API key
+            url: 'https://492aaa85-1bc1-4231-9b50-04cf0629dbc0-bluemix.cloudantnosqldb.appdomain.cloud', // Replace with your Cloudant URL
         });
         const db = cloudant.use('dealerships');
         console.info('Connect success! Connected to DB');
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
 # from .models import related models
-# from .restapis import related methods
+from .restapis import get_dealers_from_cf
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from datetime import datetime
@@ -100,11 +100,20 @@ def registration_request(request):
             return render(request, 'djangoapp/registration.html', context)
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
+#def get_dealerships(request):
+#    # context = req.get(https://sdyeung-3000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get)
+#    context = {}
+#    if request.method == "GET":
+#        return render(request, 'djangoapp/index.html', context)
+
 def get_dealerships(request):
-    # context = req.get(https://sdyeung-3000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get)
-    context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+        url = "https://sdyeung-3000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
+        # Get dealers from the URL
+        dealerships_list = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        # Return a list of dealer short name
+        return render(request, 'djangoapp/index.html', {"dealerships_list": dealerships_list})
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
